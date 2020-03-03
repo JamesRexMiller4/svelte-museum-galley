0ab9482223b89361b2f89757ede7fbfea92b3cc0
James Miller <james.rex.miller.4@gmail.com>
2020-03-02 22:28:28 -0700

    Move styles from App to Gallery component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Photo from './Photo.svelte';
+	import Gallery from './Gallery.svelte';
 	import Landing from './Landing.svelte';
 
 	let user = {hasEntered: true};
@@ -13,32 +13,8 @@
 {#if user.hasEntered}
 	<Landing on:toggle={handleToggle} user={user}/>
 {:else}
-	<main>
-		<header>
-			<h1>Harvard's Art Museum Collection</h1>
-		</header>
-		<Photo />
-	</main>
+	<Gallery />
 {/if}
 
 <style>
-	main {
-		width: 100vw;
-		height: 100vh;
-		margin: 0;
-		background-image: url('../images/art-gallery-background-img.jpg');
-		background-repeat: no-repeat;
-		background-size: cover;
-	}
-
-	header {
-		height: 20vh;
-	}
-
-	h1 {
-		font-family: 'Open Sans';
-		color: white;
-		font-size: 2.5rem;
-		padding: 40px;
-	}
 </style>
--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Photo from './Photo.svelte';
 </script>
 
 <main>
@@ -9,4 +10,23 @@
 </main>
 
 <style>
+  main {
+      width: 100vw;
+      height: 100vh;
+      margin: 0;
+      background-image: url('../images/art-gallery-background-img.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+
+	header {
+		height: 20vh;
+	}
+
+	h1 {
+		font-family: 'Open Sans';
+		color: white;
+		font-size: 2.5rem;
+		padding: 40px;
+	}
 </style>
