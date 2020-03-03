43aa43e676b42e1abd7b7b9ced4b89d580f63139
James Miller <james.rex.miller.4@gmail.com>
2020-03-02 23:23:38 -0700

    Pass props down to Gallery component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,18 @@
 	import Gallery from './Gallery.svelte';
 	import Landing from './Landing.svelte';
 
+	let promise = getPhotos();
+
+  async function getPhotos() {
+    const res = await fetch(`https://api.harvardartmuseums.org/image?apikey=API_KEY`);
+		const body = await res.json();
+		if (res.ok) {
+			return body;
+		} else {
+			throw new Error('Fetch failed');
+		}
+	}
+
 	let user = {hasEntered: true};
 
 	function handleToggle(event) {
@@ -13,7 +25,13 @@
 {#if user.hasEntered}
 	<Landing on:toggle={handleToggle} user={user}/>
 {:else}
-	<Gallery />
+	{#await promise}
+		<p>...waiting</p>
+	{:then body}
+		<Gallery records={body.records}/>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
 {/if}
 
 <style>
