a0b5b5feb041a1b6195cb49ac9d7653741dad3a1
James Miller <james.rex.miller.4@gmail.com>
2020-03-02 23:25:04 -0700

    Modify Photo to accept props

--- src/Photo.svelte
+++ src/Photo.svelte
@@ -1,31 +1,11 @@
 <script>
-  let src;
-  let promise = getPhotos();
-  async function getPhotos() {
-    const res = await fetch(`https://api.harvardartmuseums.org/image?apikey=API_KEY`);
-		const body = await res.json();
-    src = body.records[0].baseimageurl
-		if (res.ok) {
-			return src;
-		} else {
-			throw new Error('Fetch failed');
-		}
-	}
+  export let photoUrl;
+  let src = photoUrl;
 
 </script>
 
+  <img {src} alt='gallery-collection'/>
 
-<section>
-  {#await promise}
-    <p>...waiting</p>
-  {:then number}
-    <div>
-      <img {src} alt='gallery-collection'/>
-    </div>
-  {:catch error}
-    <p style="color: red">{error.message}</p>
-  {/await}
-</section>
 
 
 <style>
@@ -34,10 +14,11 @@
     height: 100vh;
     text-align: center;
   }
+
   img {
     width: 220px;
-    height: 324px;
-    margin-top: 60px;
+    height: 330px;
+    margin-top: 47px;
     margin-left: 10px;
   }
 </style>
